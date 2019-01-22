67683467589a11222bc8909bbae8c4fcb50266d8
Joris SALOPEK <joris@KitsukuPc.home>
2019-01-22 07:23:38 +0100

    dernier test pour windows

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cd build && conan install .. --build=missing -s compiler.libcxx=libstdc++11 && cmake .. -G "Unix Makefiles" && cmake --build . && cp bin/rtype-client ../ && cp bin/rtype-server ../
+mkdir build && cd build && conan install .. --build=missing -s compiler.libcxx=libstdc++11 && cmake .. -G "Unix Makefiles" && cmake --build . && cp bin/rtype-client ../ && cp bin/rtype-server ../
